f9d402db315058a0c966a498579bc00cf3335d00
Prashant Dey <prashantsavior@gmail.com>
2021-12-07 11:15:09 +0530

    ADD: First version of the login using jwt and error handling

---
+++ models.py
@@ -0,0 +1,16 @@
+from enum import unique
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://postgres:password@localhost/searchsample"
+db = SQLAlchemy(app)
+
+
+class Users(db.Model):
+    # you can even specify the table name with which you are working.
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key = True, unique=True, nullable=False)
+    name = db.Column(db.String(100), unique=False, nullable=False)
+    email = db.Column(db.String(100), unique=True, nullable=False)
+    password = db.Column(db.String(500), unique = False, nullable=False)
