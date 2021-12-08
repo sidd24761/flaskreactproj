2616f9dbd10649ab82d5c7b5e975d3c4035aad85
sidd24761 <93125984+sidd24761@users.noreply.github.com>
2021-12-08 12:32:11 +0530

    Update models.py

--- models.py
+++ models.py
@@ -3,8 +3,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://postgres:postgres@localhost/searchsample"
+app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://postgres:newpassword@localhost/searchsample"
 db = SQLAlchemy(app)
 
 
@@ -12,14 +11,27 @@ class Users(db.Model):
     # you can even specify the table name with which you are working.
     __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key = True, unique=True, nullable=False)
+    name = db.Column(db.String(100), unique=False, nullable=False)
     email = db.Column(db.String(100), unique=True, nullable=False)
     password = db.Column(db.String(500), unique = False, nullable=False)
-    name = db.Column(db.String(100), unique=False, nullable=False)
-    username=db.Column(db.String(100), unique=False, nullable=False)
+
+
 class Admins(db.Model):
     # you can even specify the table name with which you are working.
-    __tablename__ = 'Admin'
+    __tablename__ = 'admin'
     id = db.Column(db.Integer, primary_key = True, unique=True, nullable=False)
     # name = db.Column(db.String(100), unique=False, nullable=False)
     email = db.Column(db.String(100), unique=True, nullable=False)
     password = db.Column(db.String(500), unique = False, nullable=False)
+
+class Information(db.Model):
+    # you can even specify the table name with which you are working.
+    __tablename__ = 'information'
+    id = db.Column(db.Integer, primary_key = True, unique=True, nullable=False)
+    title = db.Column(db.String(100), unique=False, nullable=False)
+    tags = db.Column(db.String(100), unique=True, nullable=False)
+    categories = db.Column(db.String(500), unique = False, nullable=False)
+    link = db.Column(db.String(100), unique=False, nullable=False)
+    type = db.Column(db.String(100), unique=False, nullable=False)
+    featured = db.Column(db.String(100), unique=False, nullable=False)
+    levels = db.Column(db.String(100), unique=False, nullable=False)
